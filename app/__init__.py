f4871b3fe194b747985255d26f2d23a8aac4e9d1
phillipj1514 <phillipjunior95@gmail.com>
2020-03-11 10:40:18 -0500

    get heruku to work 1

--- app/__init__.py
+++ app/__init__.py
@@ -5,5 +5,4 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "ef322fmewfimsfeoefofdswf3o4jfemds"
 
-app.config.from_object(__name__)
 from app import views
