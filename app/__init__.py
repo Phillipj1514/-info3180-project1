8d2a17233742db7d413b037f9ebbdc57de50e53e
phillipj1514 <phillipjunior95@gmail.com>
2020-03-12 14:39:23 -0500

    the main functionalities down for step 1

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,14 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = "ef322fmewfimsfeoefofdswf3o4jfemds"
+app.config['UPLOAD_FOLDER'] = "./app/static/uploads"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:1234@localhost/profiles"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+
+app.config.from_object(__name__)
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
-from app import app
+import os
+import datetime
+from app import app,db
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.security import check_password_hash
+from werkzeug.utils import secure_filename
+from .forms  import profileForm
+from app.models import Profiles
+
 
 
 ###
@@ -12,6 +18,60 @@ from werkzeug.security import check_password_hash
 def home():
     """Render website's home page."""
     return render_template('home.html')
+
+@app.route('/about')
+def about():
+    """Render profile creation page for the website"""
+    return render_template('about.html')
+
+@app.route('/profile', methods=['POST', 'GET'])
+def profile():
+    """Render profile creation page for the website"""
+    profileform = profileForm()
+    if request.method == 'POST' and profileform.validate_on_submit():
+        fname  = profileform.first_name.data
+        lname = profileform.last_name.data
+        gender = profileform.gender.data
+        email = profileform.email.data
+        location = profileform.location.data
+        biography = profileform.biography.data
+        profileImage = profileform.image.data
+        image_name = secure_filename(profileImage.filename)
+        profileImage.save(os.path.join(
+            app.config['UPLOAD_FOLDER'], image_name
+        ))
+        create_time = datetime.datetime.now()
+
+        newProfile = Profiles(fname,lname,gender, email, location, biography, image_name, create_time)
+        db.session.add(newProfile)
+        db.session.commit()  
+        flash("profile added successfully", "success")
+        return redirect(url_for("profiles"))
+    else:
+        flash_errors(profileform)
+    return render_template('profile.html', form = profileform)
+
+@app.route('/profiles')
+def profiles():
+    """Render page to show all profiles"""
+    uprofiles = db.session.query(Profiles).all()
+    return render_template('profiles.html', profiles = uprofiles)
+
+@app.route('/profile/<id>')
+def userProfile(id):
+    """Renders the profile for a specific user ID that is entered"""
+    uprofile = Profiles.query.get(id)
+    return render_template('userprofile.html', profile = uprofile)
+
+
+# Flash errors from the form if validation fails
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" % (
+                getattr(form, field).label.text,
+                error
+            ), 'danger')
 
 ###
 # The functions below should be applicable to all Flask apps.
